2deaa2b37576bb010cc048a52ff545868cad3086
Miguel Botón <mboton@lowlevel-studios.com>
2013-09-30 10:04:59 +0200

    Initial commit.

---
+++ src/com/lowlevel/videoviewcompat/internal/PolicyManager.java
@@ -0,0 +1,50 @@
+package com.lowlevel.videoviewcompat.internal;
+
+import java.lang.reflect.Method;
+
+import android.content.Context;
+import android.view.Window;
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class PolicyManager {
+	/*
+	 * Private constants
+	 */
+    private static final String POLICY_MANAGER_CLASS_NAME = "com.android.internal.policy.PolicyManager";
+
+    /*
+     * Private variables
+     */
+    private static final Class cPolicyManager;
+
+    
+    static {
+        try {
+        	/* Get policy manager class */
+			cPolicyManager = Class.forName(POLICY_MANAGER_CLASS_NAME);
+        }
+        catch (ClassNotFoundException e) {
+            throw new RuntimeException(POLICY_MANAGER_CLASS_NAME + " could not be loaded", e);
+        }
+    }
+
+    
+    private PolicyManager() {}
+
+    
+    /*
+     * Public methods
+     */
+    public static Window makeNewWindow(Context context) {
+    	try {
+	    	/* Find method */
+			Method m = cPolicyManager.getMethod("makeNewWindow", Context.class);
+	    	
+	    	/* Invoke method */
+	        return (Window)m.invoke(null, context);
+    	}
+    	catch (Exception e) {
+    		throw new RuntimeException(POLICY_MANAGER_CLASS_NAME + ".makeNewWindow could not be invoked", e);
+    	}
+    }
+}
